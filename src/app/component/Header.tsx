57d995c8489dbf9ad87bbf973de933687a4ce916
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-07-05 20:35:31 +0900

    First commit

---
+++ src/app/component/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            메인
+        </header>
+    )
+}
---
+++ src/app/component/Navigator.tsx
@@ -0,0 +1,7 @@
+export default function Navigator() {
+    return (
+        <nav>
+            네비게이션
+        </nav>
+    )
+}
